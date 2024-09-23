c50206638c063d267549fe13fc0880593b1b7fe8
Tony Wang <zichutonywang@gmail.com>
2024-09-22 22:45:54 -0400

    Adding NGROK support.

---
+++ Exploration/learning-ngrok+flask.py
@@ -0,0 +1,21 @@
+from dotenv import dotenv_values
+
+# Load secrets
+my_secrets = dotenv_values(".env")
+
+# import ngrok python sdk
+import ngrok
+import time
+
+# Establish connectivity
+listener = ngrok.forward(5000, authtoken=my_secrets["NGROK_ACCESS_TOKEN"])
+
+# Output ngrok url to console
+print(f"Ingress established at {listener.url()}")
+
+# Keep the listener alive
+try:
+    while True:
+        time.sleep(1)
+except KeyboardInterrupt:
+    print("Closing listener")
--- Tests/test_env_secrets.py
+++ Tests/test_env_secrets.py
@@ -6,5 +6,8 @@ my_secrets = dotenv_values(".env")
 # Print all secrets from .env file
 print(my_secrets)
 
+print("---")
+
 # Print a dedicated secret
 print(my_secrets["FACEBOOK_PAGE_ACCESS_TOKEN"])
+print(my_secrets["NGROK_ACCESS_TOKEN"])
