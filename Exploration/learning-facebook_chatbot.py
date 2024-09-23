4cd8f84e4e89a83617e43eb0db9ea3a4fda6228a
Tony Wang <zichutonywang@gmail.com>
2024-09-22 22:11:36 -0400

    Updates,

--- Exploration/learning-facebook_chatbot.py
+++ Exploration/learning-facebook_chatbot.py
@@ -11,3 +11,11 @@ app = Flask(__name__)
 
 # This is API key for Facebook messenger.
 API = "https://graph.facebook.com/LATEST-API-VERSION/me/messages?access_token="+ FACEBOOK_PAGE_ACCESS_TOKEN
+
+@app.route("/", methods=['GET'])
+def fbverify():
+    if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
+        if not request.args.get("hub.verify_token")== "<Your Verify token>":
+            return "Verification token missmatch", 403
+        return request.args['hub.challenge'], 200
+    return "Hello world", 200
