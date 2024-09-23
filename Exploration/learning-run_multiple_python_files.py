44966ae19c8201dd65aaf0f52c4971ee8f04a828
Tony Wang <zichutonywang@gmail.com>
2024-09-23 01:52:32 -0400

    Running multiple python scripts example.

---
+++ Exploration/learning-run_multiple_python_files.py
@@ -0,0 +1,33 @@
+import os
+
+working_directory = os.getcwd() 
+
+# r for "raw string"
+python_file = working_directory + r"\Exploration\learning-Hello_World.py"
+
+python_command = "python.exe " + '"' + python_file + '"'
+
+# Recommended best practice
+# Executed as a separate process within file
+
+# Import subprocess module
+import subprocess
+
+# List of commands to run
+commands = [
+    python_command,
+    python_command,
+    python_command
+]
+
+# Join commands with '&&' to run them sequentially
+cmd = " && ".join(commands)
+
+# Run the commands
+process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+
+# Get the output and error (if any)
+stdout, stderr = process.communicate()
+
+print("Output:\n", stdout)
+print("Error:\n", stderr)
