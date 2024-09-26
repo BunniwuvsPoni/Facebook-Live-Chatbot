192550ea51706be2d50b33a39ccc6d6a484aa0e0
Tony Wang <zichutonywang@gmail.com>
2024-09-25 21:03:22 -0400

    Pointing to Facebook verify token.

--- Modules/flask_facebook.py
+++ Modules/flask_facebook.py
@@ -5,6 +5,7 @@ import json, requests
 # Load secrets
 my_secrets = dotenv_values(".env")
 FACEBOOK_PAGE_ACCESS_TOKEN = my_secrets["FACEBOOK_PAGE_ACCESS_TOKEN"]
+FACEBOOK_VERIFY_TOKEN = my_secrets["FACEBOOK_VERIFY_TOKEN"]
 
 # This is API key for Facebook messenger.
 API = "https://graph.facebook.com/LATEST-API-VERSION/me/messages?access_token="+ FACEBOOK_PAGE_ACCESS_TOKEN
@@ -16,7 +17,7 @@ app = Flask(__name__)
 @app.route("/", methods=['GET'])
 def fbverify():
     if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
-        if not request.args.get("hub.verify_token")== "<Your Verify token>":
+        if not request.args.get("hub.verify_token") == FACEBOOK_VERIFY_TOKEN:
             return "Verification token missmatch", 403
         return request.args['hub.challenge'], 200
     return "Hello world", 200
